b97861797817db0f89694a1bbac7f18ffe563e0f
arosecra <aaron.rosecrans@gmail.com>
2022-11-06 23:17:15 -0600

    Update Main20.java

--- src/main/java/org/github/arosecra/brooke/Main20.java
+++ src/main/java/org/github/arosecra/brooke/Main20.java
@@ -2,6 +2,8 @@ package org.github.arosecra.brooke;
 
 import java.io.File;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 
 import org.apache.commons.io.FileUtils;
 import org.apache.commons.io.FilenameUtils;
@@ -16,12 +18,18 @@ public class Main20 {
 	private static void listItemsWithoutEnglishVtt(File remoteDir, String extension) {
 		java.util.Collection<File> remoteFiles = FileUtils.listFiles(remoteDir, null, true);
 		
+		List<File> missingMkvs = new ArrayList<>();
+		
 		for(File file : remoteFiles) {
 			if(file.getName().endsWith(extension)) {
 				File english = new File(file.getParentFile(), FilenameUtils.getBaseName(file.getName()) + ".mkv");
 				if(!english.exists())
-					System.out.println(file.getParentFile() + " is missing vtt");
+					missingMkvs.add(english);
 			}
+		}
+		
+		for(int i = 0; i < missingMkvs.size(); i++) {
+			System.out.println((i+1) + ": " + missingMkvs.get(i).getParentFile() + " is missing mkv");
 		}
 	}
 }
