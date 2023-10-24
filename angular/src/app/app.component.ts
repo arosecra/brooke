315a7c3f08279c9c32f316690d2d034844400ece
arosecra <aaron.rosecrans@gmail.com>
2023-10-23 22:15:05 -0500

    standalone conversion pt2

--- angular/src/app/app.component.ts
+++ angular/src/app/app.component.ts
@@ -1,9 +1,13 @@
 import { Component, inject } from '@angular/core';
 import { BrookeService } from './brooke.service';
+import { PanelComponent } from './panel/panel.component';
+import { TopBarComponent } from './top-bar/top-bar.component';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html'
+    selector: 'app-root',
+    templateUrl: './app.component.html',
+    standalone: true,
+    imports: [TopBarComponent, PanelComponent]
 })
 export class App {
   title = 'brooke';
--- angular/src/main.ts
+++ angular/src/main.ts
@@ -1,7 +1,22 @@
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 
-import { AppModule } from './app/app.module';
+
+import { importProvidersFrom } from '@angular/core';
+import { App } from './app/app.component';
+import { withInterceptorsFromDi, provideHttpClient } from '@angular/common/http';
+import { provideAnimations } from '@angular/platform-browser/animations';
+import { BrowserModule, bootstrapApplication } from '@angular/platform-browser';
+import { BrookeServerService } from './app/brookeserver.service';
+import { BrookeService } from './app/brooke.service';
 
 
-platformBrowserDynamic().bootstrapModule(AppModule)
+bootstrapApplication(App, {
+    providers: [
+        importProvidersFrom(BrowserModule),
+        BrookeService,
+        BrookeServerService,
+        provideAnimations(),
+        provideHttpClient(withInterceptorsFromDi())
+    ]
+})
   .catch(err => console.error(err));
