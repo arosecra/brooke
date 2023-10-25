7cfeed9904acf0ae9dd288ab6d5889c59f60f941
arosecra <aaron.rosecrans@gmail.com>
2023-10-25 00:05:20 -0500

    interim commit - replacing css with custom classes

--- angular/src/app/category-and-series-browser/category-and-series-browser.component.ts
+++ angular/src/app/category-and-series-browser/category-and-series-browser.component.ts
@@ -1,5 +1,5 @@
 import { Component, inject } from '@angular/core';
-import { CommonModule, NgFor } from '@angular/common';
+import { CommonModule, NgFor, NgStyle } from '@angular/common';
 import { BrookeService } from '../brooke.service';
 import { ItemComponent } from '../item/item.component';
 import { JobDetailsComponent } from '../job-details/job-details.component';
@@ -8,7 +8,7 @@ import { JobDetailsComponent } from '../job-details/job-details.component';
     selector: 'category-and-series-browser',
     templateUrl: './category-and-series-browser.component.html',
     standalone: true,
-    imports: [JobDetailsComponent, NgFor, ItemComponent]
+    imports: [NgStyle, JobDetailsComponent, NgFor, ItemComponent]
 })
 export class CategoryAndSeriesBrowserComponent {
 	public brookeService: BrookeService = inject(BrookeService)
