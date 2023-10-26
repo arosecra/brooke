6285c2281fe24473a87b8e69f9576ba899bd797b
arosecra <aaron.rosecrans@gmail.com>
2023-10-25 23:03:25 -0500

    updated styling

--- angular/src/app/category-and-series-browser/category-and-series-browser.component.ts
+++ angular/src/app/category-and-series-browser/category-and-series-browser.component.ts
@@ -1,5 +1,5 @@
 import { Component, inject } from '@angular/core';
-import { CommonModule, NgFor, NgStyle } from '@angular/common';
+import { CommonModule, NgClass, NgFor, NgStyle } from '@angular/common';
 import { BrookeService } from '../brooke.service';
 import { ItemComponent } from '../item/item.component';
 import { JobDetailsComponent } from '../job-details/job-details.component';
@@ -8,7 +8,7 @@ import { JobDetailsComponent } from '../job-details/job-details.component';
     selector: 'category-and-series-browser',
     templateUrl: './category-and-series-browser.component.html',
     standalone: true,
-    imports: [NgStyle, JobDetailsComponent, NgFor, ItemComponent]
+    imports: [NgStyle, NgClass, JobDetailsComponent, NgFor, ItemComponent]
 })
 export class CategoryAndSeriesBrowserComponent {
 	public brookeService: BrookeService = inject(BrookeService)
