d9a1967640609f5137e3f8111715605d06797059
arosecra <aaron.rosecrans@gmail.com>
2024-05-03 16:41:33 -0500

    added missing files, updated to use experimental zoneless change detection

---
+++ angular/src/app/app.config.ts
@@ -0,0 +1,32 @@
+import {
+  ApplicationConfig,
+  importProvidersFrom,
+  provideExperimentalZonelessChangeDetection,
+  provideZoneChangeDetection,
+} from '@angular/core';
+import { provideRouter } from '@angular/router';
+
+import { routes } from './app.routes';
+import { BrowserModule } from '@angular/platform-browser';
+import { provideAnimations } from '@angular/platform-browser/animations';
+import {
+  provideHttpClient,
+  withInterceptorsFromDi,
+} from '@angular/common/http';
+import { BrookeServerService } from './brookeserver.service';
+import { BrookeService } from './brooke.service';
+
+export const appConfig: ApplicationConfig = {
+  providers: [
+    importProvidersFrom(BrowserModule),
+    BrookeService,
+    BrookeServerService,
+    provideAnimations(),
+    provideHttpClient(withInterceptorsFromDi()),
+
+    // provideZoneChangeDetection({ eventCoalescing: true }),
+    provideRouter(routes),
+
+		provideExperimentalZonelessChangeDetection(),
+  ],
+};
