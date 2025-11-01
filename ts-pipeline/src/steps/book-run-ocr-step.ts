a75fe93030ae6f1ad62360c376ac55828e21cde0
arosecra <34662069+arosecra@users.noreply.github.com>
2025-11-01 14:59:18 -0500

    Update book-run-ocr-step.ts

--- ts-pipeline/src/steps/book-run-ocr-step.ts
+++ ts-pipeline/src/steps/book-run-ocr-step.ts
@@ -2,22 +2,40 @@ import { JobFolder } from "../model/job-folder";
 import { JobStep } from "../model/job-step";
 import { Pipeline } from "../model/pipeline";
 import * as fs from 'fs';
+import * as path from 'path';
 import { node } from "../util/node";
 
 export class BookRunOcrStep implements JobStep {
 	execute(pipeline: Pipeline, job: JobFolder): void {
-		fs.readdirSync(job.destFolder).filter((file) => file.endsWith('.png')).forEach((file) => {
-			node.execFileSync(
-				"D:\Software\Python\Python313\Scripts\uv.exe",
-				[ 'run', 'mineru',
-					'-p', node.pathJoin(job.destFolder, file),
-					'-o', job.tempFolder,
-					'-l', 'en',
-					'--source', 'local'
-				], {
-				 cwd: "D:\\Projects\\mineru"
+		fs.readdirSync(job.destFolder).filter((file) => file.includes('-1-')).forEach((file) => {
+			let p = node.pathJoin(job.tempFolder, path.basename(file).replace('.png', ''))
+			console.log(p, fs.existsSync(p))
+			if(!fs.existsSync(p)) {
+				let out = '';
+				try {
+					out = node.execFileSync(
+					"C:\\Software\\Python\\Python313\\Scripts\\uv.exe",
+					[ '-q', 
+						'--directory', "C:\\Software\\mineru",
+						'run', 'mineru',
+						'-o', job.tempFolder, //
+						'-l', 'en', //
+						'-d', 'cpu', //
+						'-b', 'pipeline', //
+						'-m', 'auto', //
+						'--source', 'local', //
+						'-p', node.pathJoin(job.destFolder, file),
+					], {
+						shell: true
+					}
+					);
+					console.log(String(out));
+				} catch (err) {
+					console.error(String(out));
 				}
-			)
+			}
+
+		});
 
 			//once done, base64 intern figures
 			//     will have to re-develop
@@ -25,7 +43,6 @@ export class BookRunOcrStep implements JobStep {
 
 			//or will have to use json files to rebuild the
 			//   markdown - that's an option
-		});
 	}
 	
 }
