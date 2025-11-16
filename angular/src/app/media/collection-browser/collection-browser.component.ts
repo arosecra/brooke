6430649136916d48895f4c15b299a8acd12f0e5a
arosecra <aaron.rosecrans@gmail.com>
2025-11-16 00:49:59 -0600

    centered collection browser

--- angular/src/app/media/collection-browser/collection-browser.component.ts
+++ angular/src/app/media/collection-browser/collection-browser.component.ts
@@ -10,63 +10,7 @@ import { ActionComponent } from '../../shared/action';
 @Component({
   selector: 'collection-browser',
   imports: [ItemCardComponent, MatButtonModule, MatChipsModule, MatIconModule, MatButtonModule, SeriesComponent, ActionComponent],
-  template: `
-		@let appState = app.appState();
-		@let resources = app.resources();
-    @if (appState && resources && resources.storedLibrary.hasValue()) {
-      @let library = resources.storedLibrary.value();
-      @let currentCollection = appState.currentCollection();
-      @let currentCategory = appState.currentCategory();
-      @let currentSeries = appState.currentSeries();
-
-      @if (!currentCollection) {
-        <h2>Collections</h2>
-        <div class="flex">
-          @for (collection of library.collections; track collection.name) {
-						<action tonal="{{ collection.name.replaceAll('_', ' ') }}" [m]="app.openCollection" [p]="[collection]"></action>
-            <!-- <button matButton="tonal" (click)="app.openCollection(collection)">
-              {{ collection.name.replaceAll('_', ' ') }}
-            </button> -->
-          }
-        </div>
-      }
-
-      @if (currentCollection && !currentCategory) {
-        <h2>Categories</h2>
-        <div class="flex">
-          @for (category of library.categories; track $index) {
-            @if (category.collectionName === currentCollection.name) {
-							<action tonal="{{ category.name.replaceAll('_', ' ') }}" [m]="app.openCategory" [p]="[category]"></action>
-              <!-- <button matButton="tonal" >
-                {{ category.name.replaceAll('_', ' ') }}
-              </button> -->
-            }
-          }
-        </div>
-      }
-
-      @if (currentCollection && currentCategory && !currentSeries) {
-        <div class="flex" 
-					[class.books]="currentCollection.openType === 'book'" 
-				  [class.videos]="currentCollection.openType !== 'book'">
-          @for (itemRef of currentCategory.items; track itemRef.name) {
-						@let itemCollectionAndName = currentCollection.name + '_' + itemRef.name;
-            <item-card [itemRef]="itemRef" [item]="library.itemsByCollectionAndName[itemCollectionAndName]"
-							[thumbnail]="appState.currentCategoryThumbnails()[itemRef.name]"
-						></item-card>
-          }
-        </div>
-      }
-
-			@if(currentCollection && currentSeries) {
-				@let seriesCollectionAndName = currentCollection!.name + '_' + app.appState().currentSeries()?.name;
-				@let seriesItem = library!.itemsByCollectionAndName[seriesCollectionAndName];
-				<series-card [seriesItemRef]="currentSeries" [seriesItem]="seriesItem"
-					[thumbnail]="appState.currentCategoryThumbnails()[seriesItem.name]"
-				></series-card>
-			}
-		}
-  `,
+  templateUrl: './collection-browser.component.html', 
   styles: ``,
   encapsulation: ViewEncapsulation.None,
 })
