1d119ed728d9ebc2cf66affa8bea2c75d68d9f05
arosecra <aaron.rosecrans@gmail.com>
2025-11-23 22:14:53 -0600

    added stats, some cleanup

--- angular/src/app/media/lectern/lectern.component.ts
+++ angular/src/app/media/lectern/lectern.component.ts
@@ -1,11 +1,5 @@
 import { Component, computed, inject } from '@angular/core';
-import { FormsModule } from '@angular/forms';
-import { MatButtonModule } from '@angular/material/button';
-import { MatButtonToggleModule } from '@angular/material/button-toggle';
 import { MatCardModule } from '@angular/material/card';
-import { MatFormFieldModule } from '@angular/material/form-field';
-import { MatIconModule } from '@angular/material/icon';
-import { MatInputModule } from '@angular/material/input';
 import { AppComponent } from '../../app.component';
 import { VirtualPageComponent } from '../virtual-page/virtual-page.component';
 
@@ -13,12 +7,6 @@ import { VirtualPageComponent } from '../virtual-page/virtual-page.component';
   selector: 'lectern',
   imports: [
     MatCardModule,
-    MatButtonModule,
-    MatButtonToggleModule,
-    MatIconModule,
-    FormsModule,
-    MatFormFieldModule,
-    MatInputModule,
     VirtualPageComponent,
   ],
   host: {
