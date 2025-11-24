eeee6e760bd95f50466885688003b6c96cf8f610
arosecra <aaron.rosecrans@gmail.com>
2025-11-23 22:34:32 -0600

    upgraded ngx-remark and removed katex

--- angular/src/app/media/gallery/gallery.component.ts
+++ angular/src/app/media/gallery/gallery.component.ts
@@ -1,14 +1,13 @@
 import { Component, inject } from '@angular/core';
-import { AppComponent } from '../../app.component';
-
 import { FormsModule } from '@angular/forms';
 import { MatButtonModule } from '@angular/material/button';
 import { MatButtonToggleModule } from '@angular/material/button-toggle';
 import { MatCardModule } from '@angular/material/card';
-import { MatIconModule } from '@angular/material/icon';
-import { Page, PageType } from '../../model/page';
 import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatIconModule } from '@angular/material/icon';
 import { MatInputModule } from '@angular/material/input';
+import { AppComponent } from '../../app.component';
+import { Page, PageType } from '../../model/page';
 
 @Component({
   selector: 'gallery',
